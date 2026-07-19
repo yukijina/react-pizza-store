6dc2fd694e94b40a2076d092a9a2b780e32c6319
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-19 13:57:09 -0700

    added Error component to parent and Menu componen(invalid api call)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import Error from './ui/Error';
 
 // No need to set page not found. THere is a special way to set up.
 const router = createBrowserRouter([
   {
-    // No need to add path - parent. Layout route.
+    // No need to add path in parent(Layout route).
     element: <AppLayout />,
+    // error for all invalid url
+    errorElement: <Error />,
     // nested route. Also need to set Outlet in AppLayout to deplay children
     children: [
       {
@@ -22,6 +25,8 @@ const router = createBrowserRouter([
         element: <Menu />,
         // react route, once it goes to /menu url, it will load - fetch data
         loader: menuLoader,
+        // error for api loading. We want this error to be nested. So We add to this line as well
+        errorElement: <Error />,
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
