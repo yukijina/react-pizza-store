9a11f665545a7d26c633b20bb6dee8868e08f1a6
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-19 10:46:22 -0700

    imported AppLayout, mede other routes to childern. created loader to fetch menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
@@ -8,16 +9,25 @@ import Order from './features/order/Order';
 // No need to set page not found. THere is a special way to set up.
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
+    // No need to add path - parent. Layout route.
+    element: <AppLayout />,
+    // nested route. Also need to set Outlet in AppLayout to deplay children
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        // react route, once it goes to /menu url, it will load - fetch data
+        loader: menuLoader,
+      },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: 'order/:orderId', element: <Order /> },
+    ],
   },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  { path: '/cart', element: <Cart /> },
-  { path: '/order/new', element: <CreateOrder /> },
-  { path: 'order/:orderId', element: <Order /> },
 ]);
 
 function App() {
