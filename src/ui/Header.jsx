55f6c7265a0eb46ee5827923df0602d02d6297e3
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-20 14:42:18 -0700

    imported searchorder component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
       <Link to='/'>Fast React Pizza Co.</Link>
-      <p>Header content</p>
+      <SearchOrder />
     </header>
   );
 }
