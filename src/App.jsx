e129fdc8ad5465273047928afe45f0784b8a15b4
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-20 15:24:27 -0700

    import create order action

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import AppLayout from './ui/AppLayout';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import Error from './ui/Error';
 
@@ -29,7 +31,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: '/cart', element: <Cart /> },
-      { path: '/order/new', element: <CreateOrder /> },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
       //Place: for id
       {
         path: 'order/:orderId',
