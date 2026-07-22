66cb5f6051431e598b503642bb5199253d597c40
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-22 15:42:53 -0700

    add flex box

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">User name</div>;
+  return <div className="hidden text-sm font-semibold md:block">User name</div>;
 }
 
 export default Username;
