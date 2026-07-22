0f7e56b5eded32b339200756f3cd71fedd63837b
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-22 15:30:28 -0700

    add css for text, color, padding and margin

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="'text-stone-300 space-x-4 font-semibold sm:space-x-6">
         <span>In the Cart: </span>
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to='/cart'>Open cart &rarr;</Link>
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
