a04582058284dc59dfe0e47b66a3914be67eaed2
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-22 16:15:44 -0700

    style likn and button

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -27,12 +27,17 @@ const fakeCart = [
 function Cart() {
   return (
     <div>
-      <Link to='/menu'>&larr; Back to menu</Link>
+      <Link
+        to="/menu"
+        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+      >
+        &larr; Back to menu
+      </Link>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to='/order/new'>Order pizza</Link>
+        <Link to="/order/new">Order pizza</Link>
         <button>Clear Cart</button>
       </div>
     </div>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,33 +1,33 @@
 // import { useState } from 'react';
 
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { craeteOrder } from '../../services/apiRestaurant';
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { craeteOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -38,7 +38,7 @@ function CreateOrder() {
   // we use navagation to check loading status.
   // Status change to 'submitting' when it fetch data
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   // from react-router. It is connected to action. If there is an error in actin, it immediately come here.
   const formErrors = useActionData();
@@ -51,42 +51,45 @@ function CreateOrder() {
       <h2>Ready to order? Let's go!</h2>
 
       {/* this Form is from react-router-dom. actio function at the bottom. no need to set up on change state */}
-      <Form method='POST'>
+      <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type='text' name='customer' required />
+          <input type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type='tel' name='phone' required />
+            <input type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
           <label>Address</label>
-          <input type='text' name='address' required />
+          <input type="text" name="address" required />
         </div>
 
         <div>
           <input
-            type='checkbox'
-            name='priority'
-            id='priority'
+            type="checkbox"
+            name="priority"
+            id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
 
-          <label htmlFor='priority'>Want to give your order priority?</label>
+          <label htmlFor="priority">Want to give your order priority?</label>
         </div>
 
         <div>
           {/* This is invisible. Added this because we want to send data when the form is submitted */}
-          <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmitting}
+            className="py3 in line-block ctive: rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
       </Form>
@@ -105,7 +108,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === "on",
   };
   // console.log(order);
 
@@ -113,16 +116,16 @@ export async function action({ request }) {
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please give us your correct number. We might need it to contact you.';
+      "Please give us your correct number. We might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
-  const newOrder = await craeteOrder(order);
+  //const newOrder = await craeteOrder(order);
   // console.log(newOrder);
 
   // redirect is from react-router. We can't call Hooks (useNavigate) inside the function
-  return redirect(`/order/${newOrder.id}`);
+  //return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
