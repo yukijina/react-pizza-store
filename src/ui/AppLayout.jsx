391ffdbea104d3550529fabbd7412ea20f8914e3
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-22 15:58:32 -0700

    add css grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,26 @@
-import Header from './header';
-import CartOverview from '../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
-import Loader from './Loader';
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
   // we use navagation to check loading status.
   // Status change to 'loading' when it fetch data
   const navigation = useNavigation();
   // console.log(navigation);
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
-    <div className='layout'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-        <h1>AppLayout Content</h1>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
