0a5f8b57ef6db769dd0907d6de2f296c23b5ea79
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-23 15:23:55 -0700

    style input

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -54,24 +54,25 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
           <label>Address</label>
-          <input type="text" name="address" required />
+          <input className="input" type="text" name="address" required />
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type='text'
-        placeholder='Search Order#'
+        type="text"
+        placeholder="Search Order#"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="placeholder:color-stone-400 focus: w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm ring-yellow-500 transition-all duration-300 focus:outline-none focus:ring focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
