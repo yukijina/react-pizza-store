98c4be2ec99f7e3e1c575e00624dca7f5fbfda6d
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-24 15:56:12 -0700

    updated theme and extend for font family, color and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disablt-next line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // overwrite default setting
   theme: {
-    extend: {},
+    fontFamily: {
+     sans: 'Roboto Mono, monospace',
+    },
+    // extend - add options
+    extend: {
+      colors: {
+        pizza: '#123456',
+      },
+      height: {
+        screen: '100dvh'
+      }
+    },
   },
   plugins: [],
 };
+
+
