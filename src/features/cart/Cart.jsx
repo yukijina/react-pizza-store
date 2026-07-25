b2f9897afc0c3f7a0122e87c99649ffaf1e4948c
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-25 10:45:00 -0700

    add style and created secondary Button

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -27,17 +28,24 @@ const fakeCart = [
 ];
 
 function Cart() {
-  return (
-    <div>
-      <LinkButton to="/menu"> &larr; Back to menu</LinkButton>
-      <h2>Your cart, %NAME%</h2>
+  const cart = fakeCart;
 
-      <div>
+  return (
+    <div className="px-3 py-4">
+      <LinkButton to="/menu"> &larr; Back to menu</LinkButton>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+      <div className="mt-6 space-x-2">
         {/* <Link to="/order/new">Order pizza</Link> */}
         <Button to="/order/new" type="primary">
           Order pizza
         </Button>
-        <button>Clear Cart</button>
+        <Button type="secondary">Clear Cart</Button>
       </div>
     </div>
   );
