261c9ba772e5513f4ffa3506fe224516bf0a589b
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-26 11:58:49 -0700

    update css/style

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block ctive: rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     // need to add one space before className because it add after base
     primary: base + " px-4 py-3 md:px-6 sm:py-4",
     small: base + " px-4 py-2 md:px-5 sm:py-2.5 text-xs",
     secondary:
-      "inline-block ctive: rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 sm:py-4",
+      "inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 sm:py-4",
   };
 
   if (to)
