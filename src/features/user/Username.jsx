a6359379bbdd6f7ff9aa99d659f0c09ce7acc460
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-27 14:47:27 -0700

    get username through redux useSelector

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,14 @@
+// useSelector - get parameter of all state
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">User name</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
