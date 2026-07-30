e05a80cd2ea880a49c965a7ab571cb1c49fd679e
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-30 16:25:29 -0700

    add dispatch for testing geolocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,12 +3,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { craeteOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCart, clearCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 
@@ -58,6 +59,8 @@ function CreateOrder() {
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
+  const dispatch = useDispatch();
+
   if (!cart.length) return <EmptyCart />;
 
   //const cart = fakeCart;
@@ -65,6 +68,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>get posision</button>
 
       {/* this Form is from react-router-dom. actio function at the bottom. no need to set up on change state */}
       <Form method="POST">
