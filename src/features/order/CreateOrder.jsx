972c71f06708e30f1ea9f8e038f33511ef9fe9e9
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-30 17:46:47 -0700

    add geolocation button and get location through the user's location. Display error message if location is disabled

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -49,7 +49,15 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAddress = addressStatus === "loading";
 
   // from react-router. It is connected to action. If there is an error in actin, it immediately come here.
   const formErrors = useActionData();
@@ -68,8 +76,6 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
-      <button onClick={() => dispatch(fetchAddress())}>get posision</button>
 
       {/* this Form is from react-router-dom. actio function at the bottom. no need to set up on change state */}
       <Form method="POST">
@@ -96,10 +102,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-2 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
-          <input className="input grow" type="text" name="address" required />
+          <input
+            className="input grow"
+            type="text"
+            name="address"
+            disabled={isLoadingAddress}
+            defaultValue={address}
+            required
+          />
+
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[35px] z-50 sm:top-[1px] md:top-[5px]">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                get posision
+              </Button>
+            </span>
+          )}
         </div>
+
+        {addressStatus === "error" && (
+          <p className="mb-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+            {errorAddress}
+          </p>
+        )}
 
         <div className="items-ctner mb-12 flex gap-5">
           <input
@@ -120,7 +154,17 @@ function CreateOrder() {
           {/* This is invisible. Added this because we want to send data when the form is submitted */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
-          <Button disabled={isSubmitting} type="primary">
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}}`
+                : ""
+            }
+          />
+
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting
               ? "Placing order..."
               : `Order now for ${formatCurrency(totalPrice)}`}
@@ -144,7 +188,7 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "true",
   };
-  // console.log(order);
+  console.log(order);
 
   // Error handling
   const errors = {};
