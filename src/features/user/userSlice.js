e33f2fe40a8b13d8b31dd0b6cf29643cfdad671c
yuki <38411009+yukijina@users.noreply.github.com>
2026-07-30 17:45:59 -0700

    updated error message

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -80,7 +80,8 @@ const userSlice = createSlice({
       })
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = "error";
-        state.error = action.error.message;
+        state.error =
+          "There was a problem getting your address. Make sure to fill this filed.";
       }),
 });
 // console.log(userSlice);
